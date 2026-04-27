5129e4b023cc45cf100f1c375b5d961795313716
Nelson Schneider <65985277+bossnelnel@users.noreply.github.com>
2026-04-27 10:28:08 -0500

    initialize BankingProgram

---
+++ BankingProgram.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+
+int main()
+{
+    double balance {0.0};
+
+    /*
+     *   while (makeSelection != 4)
+ {
+ switch(makeSelection());
+    {
+        case 1:
+            displayBalance(balance);
+            break;
+        case 2:
+            balance = makeWithdrawal(balance);
+            break;
+        case 3:
+            balance = makeDeposit(balance);
+            break;
+        case 4:
+        std::cout << "Thank you for banking with us. Goodbye!\n";
+            break;
+        default:
+            std::cout << "Invalid selection. Please try again.\n";
+        
+    }
+}
+*/    
+    
+    return 0;
+}
